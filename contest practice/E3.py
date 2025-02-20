0242b6f7fa8ac966d68da1e5a257f6da698a659f
LinDragon1002 <lindragon1002@gmail.com>
2025-02-20 17:17:43 +0800

    程式訓練
    ntub_contest1
    E3完成

--- contest practice/E3.py
+++ contest practice/E3.py
@@ -1,15 +1,10 @@
 def score(a,b):
     ans = 0
-    for i in range(max(len(a),len(b))):
-        if i >= 20:
-            ans -= 5*(i-19)
-            break
-        if a[i] == "E":
-            if b[i] in "AB":
-                ans += 5
-        elif a[i] == "F":
-            if b[i] in "CD":
-                ans += 5
+    for i in range(20):
+        if a[i] == "E" and b[i] in "AB":
+            ans += 5
+        elif a[i] == "F" and b[i] in "CD":
+            ans += 5
         elif a[i] == b[i]:
             ans += 5
     return ans
