32a23c37e45e5ae8f8cedff09ddbcf4700501393
LinDragon1002 <lindragon1002@gmail.com>
2025-03-10 23:52:14 +0800

    程式競賽練習題
    P09完成

--- contest practice/七校聯盟程式競賽/P09.py
+++ contest practice/七校聯盟程式競賽/P09.py
@@ -1,9 +1,12 @@
 def counts():
     st = [i.strip() for i in input().split(",")]
-    ans = []
-    for i in range(len(st)):
-        number = int(st[i],3)
-        bin_str = bin(number)[2:]
-        ans.append(bin_str)
-    return ans
+    ans = [int(st[0],3), int(st[1],6)]
+    total = ans[0] + ans[1]
+    if total == 0:
+        return 0
+    base5 = ""
+    while total > 0:
+        base5 = str(total % 5) + base5
+        total //= 5
+    return base5
 print(counts())
