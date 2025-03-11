83683d6799409fc3ee35c48e146a93d6dc697f84
LinDragon1002 <lindragon1002@gmail.com>
2025-03-11 18:45:49 +0800

    程式訓練
    ntub_contest2
    p20

--- contest practice/ntub_contest2/p20.py
+++ contest practice/ntub_contest2/p20.py
@@ -1,10 +1,14 @@
 def distance():
     st1,st2 = input(),input()
-    st1 = [0 if i in st2 else i for i in st1]
-    temp = True
-    while temp:
-        for i in range(len(st1)):
-            if st1[i].isdigit() and st1[i+1].islower():
-                
-    return st1
+    temp = []
+    ans = []
+    for i in range(len(st1)):
+        if st1[i] == st2:
+            temp.append(i)
+    for i in range(len(st1)):
+        temp1 = []
+        for j in range(len(temp)):
+            temp1.append(abs(temp[j] - i))
+        ans.append(min(temp1))
+    return ans
 print(distance())
