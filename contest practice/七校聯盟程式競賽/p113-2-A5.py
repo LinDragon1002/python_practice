514de1f90b684117445d237c283bcde992febc1f
LinDragon1002 <lindragon1002@gmail.com>
2025-04-28 18:19:58 +0800

    程式訓練
    p113-2-A5完成

--- contest practice/七校聯盟程式競賽/p113-2-A5.py
+++ contest practice/七校聯盟程式競賽/p113-2-A5.py
@@ -2,22 +2,25 @@ num1 = int(input())
 std = [[0,"","",0,0,0] for _ in range(num1)]
 
 for i in range(num1):
-    temp = input().split(' ')
+    temp = input().split()
     for j in range(3):
         std[i][j] = temp[j]
 num2 = int(input())
 
 for i in range(num2):
-    temp = input().split(' ')
+    temp = input().split()
     for j in range(len(std)):
         if int(temp[0]) == int(std[j][0]):
             std[j][3] += int(temp[-1])
             std[j][4] += 1
 
 for i in range(len(std)):
-    std[i][5] = std[i][3] / std[i][4]
+    if std[i][4] != 0:
+        std[i][5] = std[i][3] / std[i][4]
+    else:
+        std[i][5] = 0
 
-std = sorted(std,key= lambda x:(-x[-1],x[0]))
+std = sorted(std,key= lambda x:(-x[-1],int(x[0]))) #要注意學生id是數字，但是我存在list裡面是字串
 
 for i in range(len(std)):
     print(f'{std[i][0]} {std[i][1]} {std[i][2]} {std[i][-1]:.2f}')
