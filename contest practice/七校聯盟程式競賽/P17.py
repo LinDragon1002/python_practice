493da80da136cf06341ca7d84c4f637485106351
LinDragon1002 <lindragon1002@gmail.com>
2025-05-14 14:10:02 +0800

    程式訓練
    p17完成

--- contest practice/七校聯盟程式競賽/P17.py
+++ contest practice/七校聯盟程式競賽/P17.py
@@ -10,9 +10,23 @@ for i in range(int(input())):
         temp2 = (temp[0]-1) ** 2 + 1
     if temp[0] == 1 and temp[1] == 1:
         print(1)
-    if temp1 > temp2:
-        ans = temp1 - temp[0] + 1
-        print(ans)
-    elif temp1 < temp2:
-        ans = temp2 - temp[1] + 1
+    elif temp[0] == 1:
+        print(temp1)
+    elif temp[1] == 1:
+        print(temp2)
+    else:
+        if temp1 > temp2:
+            if temp1 ** 0.5 == temp[1]:
+                ans = temp1 - temp[0] + 1
+            elif temp2 ** 0.5 == temp[0]:
+                ans = temp1 + temp[0] - 1
+            else:
+                ans = temp1 + temp[0] - 1
+        else:
+            if temp2 ** 0.5 == temp[0]:
+                ans = temp2 - temp[1] + 1
+            elif temp1 ** 0.5 == temp[1]:
+                ans = temp2 + temp[1] - 1
+            else:
+                ans = temp2 + temp[1] - 1
         print(ans)
